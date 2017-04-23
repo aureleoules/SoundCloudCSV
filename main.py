62994c1850006888001c84bdfe1648960e0ef1fb
Aurèle Oulès <aurele@oules.com>
2017-04-23 15:34:45 +0200

    Added stats at the end of process

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import soundcloud
 import csv
-client_id="YOUR CLIENT ID" #TODO: Change to your Client ID
+client_id="YOUR_CLIENT_ID" #TODO: Change to your Client ID
 username = input("Username: ")
 
 client = soundcloud.Client(client_id=client_id)
-
+totalDuration = 0
 page_size = 200 # Max 200!
 next_href = ""
 fname = username + ".csv"
 file = open(fname, "w")
 writer = csv.writer(file)
-
 def writeLine(track):
+    global totalDuration
     writer.writerow( (track.title, track.permalink_url) )
     print(track.title.replace(',', ' ') + " " + track.permalink_url)
+    totalDuration = totalDuration + track.duration
 
 tracks = client.get('/users/' + username + '/favorites', order='created_at', limit=page_size, 
 linked_partitioning=1)
@@ -31,4 +32,9 @@ while(next_href):
     else:
         next_href = ""
 print("Done.")
-    
+print("Total playlist duration:")
+print("    " + repr(round(totalDuration / 1000)) + "s.")
+print("    " + repr(round(totalDuration / 1000 / 60)) + "mins.")
+print("    " + repr(round(totalDuration / 1000 / 60 / 24)) + " days.")
+print("    " + repr(round(totalDuration / 1000 / 60 / 24 / 30)) + " months.")
+
